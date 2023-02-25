ae42c0e39eac5e06b1159bcd4b5e049861901499
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2023-02-25 16:21:39 +0000

    fixes

--- book/bookquery/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import Author, Book, BookAuthor, BookBookshelf, BookLanguage,Language, BookSubject, Bookshelf, Format
-
-# Register your models here.
-admin.site.register((Author, Book, BookAuthor, BookBookshelf, BookLanguage, Language, BookSubject, Bookshelf, Format, ))
